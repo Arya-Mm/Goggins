6b6ad2a94ea89aa3595438568956ff8094fe2d94
Arya-Mm <aryamishra828@gmail.com>
2026-02-28 19:23:23 +0000

    Mar 1, 2026, 12:53 AM

--- core/twin/twin_builder.py
+++ core/twin/twin_builder.py
@@ -16,9 +16,6 @@ class StructuralTwinBuilder:
         return math.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2)
 
     def _match_dimension(self, wall_bbox, dimensions):
-        """
-        Assign nearest dimension to wall.
-        """
         if not dimensions:
             return None
 
@@ -28,6 +25,30 @@ class StructuralTwinBuilder:
         )
 
         return nearest["inches"]
+
+    def compute_quantities(self, twin):
+        WALL_HEIGHT_INCHES = 120  # 10 ft default
+        WALL_THICKNESS_INCHES = 9  # standard brick wall
+
+        total_wall_volume_cuft = 0
+
+        for wall in twin["walls"]:
+            length = wall["length_inches"]
+
+            if length is None:
+                continue
+
+            volume_cuin = length * WALL_HEIGHT_INCHES * WALL_THICKNESS_INCHES
+            volume_cuft = volume_cuin / 1728
+
+            wall["volume_cuft"] = round(volume_cuft, 2)
+
+            total_wall_volume_cuft += volume_cuft
+
+        twin["total_wall_volume_cuft"] = round(total_wall_volume_cuft, 2)
+        twin["estimated_bricks"] = int(total_wall_volume_cuft * 13.5)
+
+        return twin
 
     def build(self, vision_output):
 
@@ -75,5 +96,7 @@ class StructuralTwinBuilder:
             "beams": objects.get("beams", []),
             "slabs": objects.get("slabs", [])
         }
+
+        twin = self.compute_quantities(twin)
 
         return twin
